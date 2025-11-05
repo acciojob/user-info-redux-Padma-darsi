2918316d7dc2f638153774b6d8ca3d595f044b71
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-11-06 02:38:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 
 import React from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+import UserInfo from "./UserInfo";
 import './../styles/App.css';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Provider store={store}>
+      <div>
+        <UserInfo />
+      </div>
+    </Provider>
+  );
+};
 
-export default App
+export default App;
